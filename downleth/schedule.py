db2cdc1157349a7418467208f1b02bb6c29be425
Thomas Gassmann <thomas.gassmann@hotmail.com>
2022-06-04 01:30:54 +0200

    Add ONCE scheduling
    Signed-off-by: Thomas Gassmann <thomas.gassmann@hotmail.com>

--- downleth/schedule.py
+++ downleth/schedule.py
@@ -19,6 +19,9 @@ async def download_room(room_id: str, download_duration: int, output_file: str, 
     logging.info(f'Done downloading {room_id}. Writing file...')
     await d.stop(output_file)
 
+PERIOD_WEEKLY = 'weekly'
+PERIOD_ONCE = 'once'
+
 class ScheduleWhen:
 
     def __init__(self, when_config, friendly_name_fn):
@@ -28,10 +31,10 @@ class ScheduleWhen:
         self._friendly_name_fn = friendly_name_fn
 
     def reached(self):
-        if self._tz_aware_now() >= self._get_next_weekly_from() and self._tz_aware_now() <= self._get_next_weekly_to():
+        if self._tz_aware_now() >= self._get_next_from() and self._tz_aware_now() <= self._get_next_to():
             return True # lecture is happening now, TODO: it is probably not
 
-        if self._get_next_weekly_from() >= self._get_next_weekly_to():
+        if self._get_next_from() >= self._get_next_to():
             return True # lecture is really happening now
 
         # yes, the below comment is useless actually
@@ -41,7 +44,7 @@ class ScheduleWhen:
         if self.reached():
             return # no need to wait
 
-        wait_delta = floor((self._get_next_weekly_from() - self._tz_aware_now()).total_seconds())
+        wait_delta = floor((self._get_next_from() - self._tz_aware_now()).total_seconds())
 
         wait_until = self._tz_aware_now() + datetime.timedelta(seconds=wait_delta)
         logging.info(f'Waiting for {self._friendly_name_fn(self)} until {wait_until}')
@@ -49,20 +52,22 @@ class ScheduleWhen:
         await asyncio.sleep(wait_delta)
 
     def next_name(self):
-        if self._schedule == 'weekly':
+        if self._schedule == PERIOD_WEEKLY:
             week = self.timeframe_from().isocalendar().week
             year = self.timeframe_from().isocalendar().year
             return f'{year}-{week}'
+        if self._schedule == PERIOD_ONCE:
+            return ''
         raise ValueError(self._schedule)
 
     def next_duration(self):
-        start = self._get_next_weekly_from()
-        if self._tz_aware_now() <= self._get_next_weekly_to() and self._tz_aware_now() >= self._get_next_weekly_from():
+        start = self._get_next_from()
+        if self._tz_aware_now() <= self._get_next_to() and self._tz_aware_now() >= self._get_next_from():
             start = self._tz_aware_now()
-        if self._get_next_weekly_from() >= self._get_next_weekly_to():
+        if self._get_next_from() >= self._get_next_to():
             start = self._tz_aware_now()
 
-        return ceil((self._get_next_weekly_to() - start).total_seconds())
+        return ceil((self._get_next_to() - start).total_seconds())
 
     def timeframe_from(self):
         return self._parse_date(self._config['timeframe']['from'], self._config['timeframe']['timezone'])
@@ -70,13 +75,16 @@ class ScheduleWhen:
     def timeframe_to(self):
         return self._parse_date(self._config['timeframe']['to'], self._config['timeframe']['timezone'])
 
-    def _get_next_weekly_from(self):
-        return self._get_next_weekly_offset(self.timeframe_from())
+    def _get_next_from(self):
+        return self._get_next_offset(self.timeframe_from())
 
-    def _get_next_weekly_to(self):
-        return self._get_next_weekly_offset(self.timeframe_to())
+    def _get_next_to(self):
+        return self._get_next_offset(self.timeframe_to())
 
-    def _get_next_weekly_offset(self, d):
+    def _get_next_offset(self, d):
+        if self._schedule == PERIOD_ONCE:
+            return d
+
         # relative to self._start
         offset = self._get_offset_delta()
         offset_seconds = floor(offset.total_seconds())
@@ -84,8 +92,10 @@ class ScheduleWhen:
         return self._start + datetime.timedelta(seconds=seconds)
 
     def _get_offset_delta(self):
-        if self._schedule == 'weekly':
+        if self._schedule == PERIOD_WEEKLY:
             return datetime.timedelta(days=7)
+        if self._schedule == PERIOD_ONCE:
+            return datetime.timedelta.max
         raise ValueError(self._schedule)
 
     def _parse_date(self, date_str, timezone):
