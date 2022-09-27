5252bc9791eaafbc09d3dfbcbf66000e67a53ffc
Thomas Gassmann <thomas@gassmann.dev>
2022-09-27 12:05:04 +0200

    Bump version
    Signed-off-by: Thomas Gassmann <thomas@gassmann.dev>

--- downleth/__init__.py
+++ downleth/__init__.py
@@ -1,4 +1,4 @@
 __author__ = 'Thomas Gassmann'
 __email__ = 'thomas@gassmann.dev'
 version_string = '#{GitVersion}#' # TODO: will probably be used in the future
-__version__ = '1.1'
+__version__ = '1.2'
